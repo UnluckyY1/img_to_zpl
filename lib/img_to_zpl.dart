9ef3d1de1500d0d553e2cbd17e6eb390ea379c37
Baaji Shayeed Shaik <baajishayeedshaik@gmail.com>
2021-07-20 16:47:23 -0500

    added camera package added comments to the package

--- lib/img_to_zpl.dart
+++ lib/img_to_zpl.dart
@@ -4,19 +4,19 @@ import 'dart:async' show Completer, Future;
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-
 import 'package:flutter/widgets.dart';
 import 'package:image/image.dart' as img;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-
+/// This class provides a method to convert an image in Uint8List format to a
+/// zpl string to print to a zebra printer
+///
 class ImageToZpl {
+  /// converts the image to zpl string in the following format
+  /// '~DF[name], [totalImageBytes], [bytesperRow], [data in hex format]\n'
+  /// you can use this image in zpl using the following command
+  /// '^FO25,0^XGR:[name],1,1^FS'
   Future<String> convertImgtoZpl(String name, Uint8List imageAsU8) async {
-    HexImageString tuple = await getHexBody(imageAsU8);
+    HexImageString tuple = await _getHexBody(imageAsU8);
     var body = tuple.hexImage;
     //if (compressHex) cuerpo = encodeHexAscii(cuerpo);
     return '~DG${name},' +
@@ -28,7 +28,7 @@ class ImageToZpl {
         "\n";
   }
 
-  Future<HexImageString> getHexBody(Uint8List imageAsU8) async {
+  Future<HexImageString> _getHexBody(Uint8List imageAsU8) async {
     var image = Image.memory(imageAsU8);
     Completer<ui.Image> completer = new Completer<ui.Image>();
     image.image
@@ -64,7 +64,7 @@ class ImageToZpl {
         colorByte[index] = auxChar;
         index++;
         if (index == 8 || w == (width - 1)) {
-          hexString += fourByteBinary(colorByte.join());
+          hexString += _fourByteBinary(colorByte.join());
           colorByte = ['0', '0', '0', '0', '0', '0', '0', '0'];
           index = 0;
         }
@@ -75,7 +75,7 @@ class ImageToZpl {
         hexImage: hexString, totalBytes: total, widthBytes: widthBytes);
   }
 
-  String fourByteBinary(String binaryStr) {
+  String _fourByteBinary(String binaryStr) {
     int decimal = int.parse(binaryStr, radix: 2);
     if (decimal > 15) {
       return decimal.toRadixString(16).toUpperCase();
