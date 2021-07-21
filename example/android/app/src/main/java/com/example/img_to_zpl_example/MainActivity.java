0e59edd167e5af8a32fb51fe66b272f8756ca7ae
Baaji Shayeed Shaik <baajishayeedshaik@gmail.com>
2021-07-21 11:01:59 -0500

    boiler plate example for plugin

---
+++ example/android/app/src/main/java/com/example/img_to_zpl_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.img_to_zpl_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import img_to_zpl
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ImgToZplPlugin.register(with: registry.registrar(forPlugin: "ImgToZplPlugin"))
+}
