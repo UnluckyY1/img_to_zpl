0f13c5682a78d928d4ce33e981174f5fea65fa76
Baaji Shayeed Shaik <baajishayeedshaik@gmail.com>
2021-07-21 11:03:38 -0500

    added the test file

--- test/img_to_zpl_test.dart
+++ test/img_to_zpl_test.dart
@@ -1,5 +1,23 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:img_to_zpl/img_to_zpl.dart';
 
-void main() {}
+void main() {
+  const MethodChannel channel = MethodChannel('img_to_zpl');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await ImgToZpl.platformVersion, '42');
+  });
+}
